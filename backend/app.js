9814356c36aec40e0fbd1bc54d86d69e1abb1c6f
Harsh Deep <deepharsh057@gmail.com>
2023-06-04 13:45:28 +0530

    updating db connection

--- backend/app.js
+++ backend/app.js
@@ -5,26 +5,31 @@ require('dotenv').config();
 
 const app = express();
 const authRoutes = require("./routes/auth");
+const connectDB = require('./config/db');
 
-app.use(cors());
-app.use(express.json());
+async function startApp() {
+    try {
+        await connectDB();
+        app.use(cors());
+        app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+        app.get('/', (req, res) => {
+            res.send("connected")
+        });
 
-mongoose.connect(process.env.MONGO_URL , {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-      console.error('Error connecting to MongoDB:', error);
-    });
+        app.use("/api/auth", authRoutes);
+        const server = app.listen(process.env.PORT, () => {
+            console.log(`server listening on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting the application:', error);
+    }
+}
 
-const server = app.listen(process.env.PORT, ()=>{
-    console.log(`server listening on port ${process.env.PORT}`);
-})
+startApp();
+
+
+
 
 
 
