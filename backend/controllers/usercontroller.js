7ffd867ab086dfb59c9f6347fb2bd50b8a466e78
dhruti591 <dhrutidesai210@gmail.com>
2023-06-05 19:04:08 +0530

    develop to handle  update user_profile and detail api

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -25,3 +25,46 @@ module.exports.register = async (req, res, next) => {
   }
 };
 
+module.exports.update_profile_pic = async (req, res, next) => {
+  console.log(req.body);
+  try{
+    const {profilePath} = req.body;
+    const user_id = req.params.user_id;
+    const user =  await User.find({_id: user_id });
+
+    if(user){
+      user[0].profile_path = profilePath;
+ 
+      await user[0].save();
+      res.status(201).json({ message: 'User profile update successfully' });
+    }
+    else{
+      res.status(400).json({msg: 'User not found'});
+    }
+  }
+  catch(ex) {
+    console.log(ex);
+  }
+}
+
+module.exports.update_profile_pic = async (req, res, next) => {
+  console.log(req.body);
+  try{
+    const {updateUser} = req.body;
+    const user_id = req.params.user_id;
+    const existingUser =  await User.find({_id: user_id });
+
+    if(existingUser){
+      existingUser[0]=updateUser;
+ 
+      await existingUser[0].save();
+      res.status(201).json({ message: 'User details update successfully' });
+    }
+    else{
+      res.status(400).json({msg: 'User not found'});
+    }
+  }
+  catch(ex) {
+    console.log(ex);
+  }
+}
